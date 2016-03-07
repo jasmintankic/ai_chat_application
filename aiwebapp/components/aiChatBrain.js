ec887ebfc4df40feb8c53b0926ec2fe3ab3f1484
Jasmin Tankic <jasmin.tankic@virginpulse.com>
2016-03-07 13:21:51 +0100

    wiki api vraca promise

--- aiwebapp/components/aiChatBrain.js
+++ aiwebapp/components/aiChatBrain.js
@@ -3,22 +3,9 @@
 
     angular
         .module('ui-chat-app')
-        .factory('wikiService', function($http) {
-    var wikiService = {
-        get: function(keyword) {
-            return $http.jsonp('https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro=&explaintext=&titles=' + keyword.name + '&callback=JSON_CALLBACK');
-        }
-    };
+        .controller('aiChatBrain', aiChatBrain);
 
-    return wikiService;
-
-})
-
-.controller('aiChatBrain', function($scope, wikiService) {
-    wikiService.get({name: 'Sarajevo'}).then(function(data) {
-    console.log(data.data);
-    });
-
-});
+    function aiChatBrain() {
+    }
 
 })();
--- aiwebapp/components/externalResources.js
+++ aiwebapp/components/externalResources.js
@@ -5,18 +5,39 @@ angular.module('ui-chat-app')
 		return $resource('http://api.openweathermap.org/data/2.5/find');
 	})
 
-	.factory('externalResourcesService', function($q, weatherInfoResource) {
-
-		var externalResourcesService = {};
-
-		externalResourcesService.getWeatherInfo = function(city) {
-			var weatherObject = {
-				q: city,
-				units: 'metric',
-				appid: 'a232c1c0480a53ff51b834e8093462b6'
-			};
-			return weatherInfoResource.get(weatherObject,{}).$promise;
-		};
-
-		return externalResourcesService;
+.factory('wikiResources', function($resource) {
+	return $resource('https://en.wikipedia.org/w/api.php', {
+		action: 'query',
+		prop: 'extracts',
+		exintro: 'explaintext',
+		format: 'json',
+		callback: 'JSON_CALLBACK'
+	}, {
+		get: {
+			method: 'JSONP'
+		}
 	});
+})
+
+.factory('externalResourcesService', function($q, weatherInfoResource, wikiResources) {
+
+	var externalResourcesService = {};
+
+	externalResourcesService.getWeatherInfo = function(city) {
+		var weatherObject = {
+			q: city,
+			units: 'metric',
+			appid: 'a232c1c0480a53ff51b834e8093462b6'
+		};
+		return weatherInfoResource.get(weatherObject, {}).$promise;
+	};
+
+	externalResourcesService.getInfoFromWiki = function(keyWord) {
+		var wikiObject = {
+			titles: keyWord
+		};
+		return wikiResources.get(wikiObject, {}).$promise;
+	};
+
+	return externalResourcesService;
+});
